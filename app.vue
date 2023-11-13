26bafbed7eb0503bd6460fb06ec9da21e512bc7d
Michal Sadowski <saski.michal.sadowski@gmail.com>
2023-11-13 15:00:51 +0100

    Inital commit

---
+++ app.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <NuxtPage />
+  </div>
+</template>
+
+<script setup>
+
+useHead({
+  script: [
+    { src: "https://identity.netlify.com/v1/netlify-identity-widget.js" },
+  ],
+});
+
+</script>
